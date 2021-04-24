46ebb189cf29988df365005ab325c5edc72af9c1
Nora Moseman <nmoseman@post.harvard.edu>
2021-04-24 13:46:51 -0600

    further testing

--- app/tests/test_in_browser.py
+++ app/tests/test_in_browser.py
@@ -28,14 +28,12 @@ class TestIndex(StaticLiveServerTestCase):
         d['goog:loggingPrefs'] = { 'browser':'ALL' }
         options = Options()
         # https://stackoverflow.com/questions/48450594/selenium-timed-out-receiving-message-from-renderer
-        options.add_argument("start-maximized"); # https://stackoverflow.com/a/26283818/1689770
-        options.add_argument("enable-automation"); # https://stackoverflow.com/a/43840128/1689770
-        options.add_argument("--headless"); # only if you are ACTUALLY running headless
-        options.add_argument("--no-sandbox"); # https://stackoverflow.com/a/50725918/1689770
-        # options.add_argument("--disable-infobars"); # https://stackoverflow.com/a/43840128/1689770
-        options.add_argument("--disable-dev-shm-usage"); # https://stackoverflow.com/a/50725918/1689770
-        # options.add_argument("--disable-browser-side-navigation"); # https://stackoverflow.com/a/49123152/1689770
-        options.add_argument("--disable-gpu"); # https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+        options.add_argument("start-maximized") # https://stackoverflow.com/a/26283818/1689770
+        options.add_argument("enable-automation") # https://stackoverflow.com/a/43840128/1689770
+        options.add_argument("--headless") # only if you are ACTUALLY running headless
+        options.add_argument("--no-sandbox") # https://stackoverflow.com/a/50725918/1689770
+        options.add_argument("--disable-dev-shm-usage") # https://stackoverflow.com/a/50725918/1689770
+        options.add_argument("--disable-gpu") # https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
         options.add_argument("window-size=1280x600")
         cls.selenium = webdriver.Chrome(chrome_options=options, desired_capabilities=d)
         cls.selenium.implicitly_wait(20)
